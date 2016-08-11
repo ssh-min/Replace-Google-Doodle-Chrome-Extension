9e4aa889c9e19d822b40b1943fb98c47f7e500a9
Shashank Swaminathan <ssashanks@gmail.com>
2016-08-11 14:09:56 +0900

    Chan

---
+++ popup.js
@@ -0,0 +1,13 @@
+
+var sendmessage = function() {
+
+};
+
+var log = function() {
+alert("alert");
+};
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById("browseimage").addEventListener("click", log);
+});
