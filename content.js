28864866e5c51584b72160887a54fbbb8e924d92
lakshman <diwaakartg@gmail.com>
2016-08-11 10:15:42 +0530

    removing the unwanted function

--- content.js
+++ content.js
@@ -1,11 +1,16 @@
-var test = function() {
-	//alert("hello");
-    custom_icon = document.createElement("img");
-    target = $("#hplogo");
-    target.empty();
-    custom_icon.setAttribute("id", "myimage");
-    custom_icon.setAttribute("style", "display:inline-block;position:relative;max-height:230px;max-width:488px;");
-    custom_icon.src = chrome.extension.getURL("tree.png");
-    target.append(custom_icon);
-};
-test();
+var custom_icon = document.createElement("img");
+var target = $("#hplogo");
+target.empty();
+custom_icon.setAttribute("id", "myimage");
+custom_icon.setAttribute("style", "display:inline-block;position:relative;max-height:230px;max-width:488px;");
+custom_icon.src = chrome.extension.getURL("tree.png");
+target.append(custom_icon);
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });
